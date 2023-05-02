bed34ae33636df628d804ab52c3709bc0ab0f11e
lucas <mr.pine97@gmail.com>
2023-05-02 17:06:13 -0300

    arquivos gerados pelo comando ng g component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListarComponent } from './pages/listar/listar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+     AppComponent,
+    HomeComponent,
+    ListarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
