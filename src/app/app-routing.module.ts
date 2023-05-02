27c3204b9b86904cc1b5a6feb95abf5fd65f6fc9
lucas <mr.pine97@gmail.com>
2023-05-02 17:06:43 -0300

    chamando com componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListarComponent } from './pages/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path:'cadastro', component: ListarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
