250f1db626b0b4c6e396dbabe188f349a5431e74
lucas <mr.pine97@gmail.com>
2023-05-02 17:44:04 -0300

    usa o header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', // isso vira uma tag
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
