e7308316e866120bde6cddce676473ec09bb6e0a
lucas <mr.pine97@gmail.com>
2023-05-10 17:56:58 -0300

    adiciona página de usuários obtendo a lista do método UserService

---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { IUser } from 'src/app/models/user.interface';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  users?: IUser[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+}
