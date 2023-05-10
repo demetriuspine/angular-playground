858b05f60484aa5586655039090ccf17659e919e
lucas <mr.pine97@gmail.com>
2023-05-10 17:57:38 -0300

    adiciona e usa rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListarComponent } from './pages/listar/listar.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ManipulandoJsonComponent } from './pages/manipulando-json/manipulando-json.component';
+import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'register', component: CadastroComponent },
   { path: 'login', component: LoginComponent },
   { path: 'students', component: ManipulandoJsonComponent },
+  { path: 'users', component: UsersComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { ManipulandoJsonComponent } from './pages/manipulando-json/manipulando-json.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { HttpClientModule } from '@angular/common/http';
     CadastroComponent,
     LoginComponent,
     MainComponent,
-    ManipulandoJsonComponent
+    ManipulandoJsonComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
