73639af8d8062949864f6161581e7479652f233e
lucas <mr.pine97@gmail.com>
2023-05-10 17:55:21 -0300

    insere logica para obter usuarios e cadastrar um usuário

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../models/user.interface';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private BASE_URL: string = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<IUser[]> {
+    const users = this.http.get<IUser[]>(`${this.BASE_URL}/users`);
+    return users;
+  }
+
+  postUser(user: IUser): Observable<IUser>{
+    const newUser = this.http.post<IUser>(`${this.BASE_URL}/users`, user, httpOptions);
+    return newUser;
+  }
 }
