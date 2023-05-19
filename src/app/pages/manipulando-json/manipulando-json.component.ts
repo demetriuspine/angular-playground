841cdab76353d1e2ee35328ba2b2bfb7724204a4
lucas <mr.pine97@gmail.com>
2023-05-19 12:29:21 -0300

    manda o parametro

--- src/app/pages/manipulando-json/manipulando-json.component.ts
+++ src/app/pages/manipulando-json/manipulando-json.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import studentsData from '../../students.json';
 import { IStudent } from 'src/app/models/student.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manipulando-json',
@@ -10,11 +11,17 @@ import { IStudent } from 'src/app/models/student.interface';
 export class ManipulandoJsonComponent {
   private students: IStudent[] = studentsData;
 
+  constructor(private router: Router) { }
+
   public getStudents(): IStudent[] {
     return this.students;
   }
 
   ngOnInit(): void {
     console.log(this.students);
+  }
+
+  goToDetail(student: IStudent) {
+    this.router.navigate(['detail', student.id]);
   }
 }
