8f99833adcf307cee9564f67d03aa2311f27690a
lucas <mr.pine97@gmail.com>
2023-05-19 12:29:05 -0300

    recebe e usa parametro

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  identifier: number = 0;
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit(): void {
+    this.route.params.forEach((param: Params) => {
+      if (param['id']) {
+        this.identifier = param['id'];
+        console.log(this.identifier);
+      }
+    })
+  }
 }
