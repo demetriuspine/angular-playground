677a743e44b38783b76ea8886e3ad5b22049a33a
lucas <mr.pine97@gmail.com>
2023-05-19 12:08:58 -0300

    usa o service de autorizacao no guard

--- src/app/guard/authorized.guard.ts
+++ src/app/guard/authorized.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizationService } from '../services/authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizedGuard implements CanActivate {
+  constructor(
+    private authorizationService: AuthorizationService,
+    private routerService: Router
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    const isUserLogged = this.authorizationService.getLoginStatus();
+    if (isUserLogged)
+      return true
+    this.routerService.navigate(['/login'])
+    return false;
   }
-  
+
 }
