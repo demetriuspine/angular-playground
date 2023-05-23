1021c6fc901a04eb400c2cdf21bfe126555d8b31
lucas <mr.pine97@gmail.com>
2023-05-23 18:28:02 -0300

    chama postUser e atualiza nome de campo

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { IUser } from 'src/app/models/user.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,7 +12,7 @@ export class CadastroComponent {
   registerForm = this.fb.group({
     id: '',
     company: null,
-    firstName: [null, Validators.required],
+    name: [null, Validators.required],
     email: ['', Validators.compose([
       Validators.required, Validators.minLength(5), Validators.maxLength(50), Validators.email],)
     ],
@@ -23,7 +25,7 @@ export class CadastroComponent {
 
   hasUnitNumber = false;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private userService: UserService) { }
 
   email = this.registerForm.controls['email'];
 
@@ -35,6 +37,19 @@ export class CadastroComponent {
   }
 
   onSubmit(): void {
+    const user: IUser = {
+      name: this.registerForm.controls['name'].value!,
+      username: this.registerForm.controls['username'].value!,
+      email: this.registerForm.controls['email'].value!,
+      password: this.registerForm.controls['password'].value!,
+      city: this.registerForm.controls['city'].value!,
+      phone: this.registerForm.controls['phone'].value!,
+      companyName: this.registerForm.controls['companyName'].value!
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    this.userService.postUser(user).subscribe(newUser => {
+      console.log(newUser);
+    });
     alert('Thanks!');
   }
 }
